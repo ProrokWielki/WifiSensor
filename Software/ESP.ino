160afb4b25c45f3605cdb0d5c011c8fd91735426
ProrokWielki <ProrokWielki@o2.pl>
2022-08-15 17:50:43 +0200

    initial software

---
+++ Software/ESP.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+
+#include <Wire.h>
+
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <RemoteDebug.h>
+
+#include <BME280I2C.h>
+
+#include "config/i2c_config.h"
+#include "esp_mqtt_helpers/config/mqtt_config.h"
+#include "esp_mqtt_helpers/config/wifi_config.h"
+
+#include "esp_mqtt_helpers/helpers/json.h"
+#include "esp_mqtt_helpers/helpers/utils.h"
+
+#define get_name(var) #var
+
+BME280I2C::Settings settings(BME280::OSR_X1, BME280::OSR_X1, BME280::OSR_X1, BME280::Mode_Forced, BME280::StandbyTime_1000ms, BME280::Filter_Off,
+                             BME280::SpiEnable_False, BME280I2C::I2CAddr::I2CAddr_0x76);
+BME280I2C bme_(settings);
+WiFiClient wifi_;
+PubSubClient mqtt_(mqtt::server_ip, mqtt::server_port, wifi_);
+
+void setup()
+{
+    Wire.begin(i2c::sda_pin, i2c::scl_pin);
+
+    WiFi.hostname(wifi::hostname);
+    WiFi.begin(wifi::ssid, wifi::password);
+
+    wait_for_initialization([]() { return not WiFi.status() == WL_CONNECTED; }, 5, []() { ESP.deepSleep(15 * 60 * 1000 * 1000); });
+    wait_for_initialization([]() { return not bme_.begin(); }, 5, []() { ESP.deepSleep(15 * 60 * 1000 * 1000); }),
+    wait_for_initialization([]() { return not mqtt_.connect(mqtt::client_id); }, 5, []() { ESP.deepSleep(15 * 60 * 1000 * 1000); });
+
+    mqtt_.publish(wifi::hostname, make_json(std::make_pair("name", wifi::hostname), std::make_pair("temperature", bme_.temp(BME280::TempUnit_Celsius)),
+                                            std::make_pair("humidity", bme_.hum()), std::make_pair("pressure", bme_.pres(BME280::PresUnit_hPa)))
+                                  .c_str());
+
+    delay(1000);
+
+    ESP.deepSleep(15 * 60 * 1000 * 1000);  // 15 min
+}
+
+void loop()
+{
+}
---
+++ Software/config/i2c_config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_I2C_CONFIG_H_
+#define CONFIG_I2C_CONFIG_H_
+
+namespace i2c
+{
+constexpr auto sda_pin = 13;
+constexpr auto scl_pin = 12;
+}  // namespace i2c
+
+#endif /* CONFIG_I2C_CONFIG_H_ */
